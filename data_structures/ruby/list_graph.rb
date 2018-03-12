e27b3084e7c8e335b7d0940524eace2722f20ebe
Ryan Mease <rsmease@gmail.com>
2018-03-12 08:22:35 -0700

    Remove dupblicate metehod from ListGraph.js

--- data_structures/ruby/list_graph.rb
+++ data_structures/ruby/list_graph.rb
@@ -1,4 +1,7 @@
 class ListGraph
+
+    attr_reader :number_of_edges
+
     def initialize
         @vertices = {}
         @edges = {}
@@ -8,6 +11,7 @@ class ListGraph
     def add_vertex(vertex)
         @vertices[vertex] = true
         @edges[vertex] = []
+        return vertex
     end
 
     def remove_vertex(vertex)
@@ -15,5 +19,32 @@ class ListGraph
         until @edges[vertex].length == 0
             remove_edge(@edges[vertex].pop(), vertex)
         end
+        return vertex
     end
+
+    def add_edge(vertex1, vertex2)
+        @edges[vertex1].push(vertex2)
+        @edges[vertex2].push(vertex2)
+        @number_of_edges += 1
+        return
+    end
+
+    def remove_edge(vertex1, vertex2)
+        @edges[vertex1].delete_at(@edges[vertex1].index(vertex2))
+        @edges[vertex2].delete_at(@edges[vertex2].index(vertex1))
+        @number_of_edges += 1
+        return
+    end
+
+    def size
+        return @vertices.size
+    end
+
+    def has_vertex(vertex)
+        return @vertices[vertex]
+    end
+
+    
+
+
 end
