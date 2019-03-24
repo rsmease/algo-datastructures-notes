623fcc5692f98f0fab17af980d16f87f41b5e768
rsmease <rsmease@gmail.com>
2019-03-24 11:53:28 -0700

    add function iterator

--- algorithms/javascript/functionalComposition.js
+++ algorithms/javascript/functionalComposition.js
@@ -10,3 +10,15 @@ const square = (x) => x * x;
 const inc = (x) => x + 1;
 
 console.log(functionalComposition(6, inc, square))
+
+const repeatFunction = (operator, iterations, operand) => {
+  let result = operand;
+  for (let i = 0; i < iterations; i++) {
+    result = operator(result);
+  }
+  return result
+}
+
+console.log(repeatFunction(square, 3, 5))
+
+
