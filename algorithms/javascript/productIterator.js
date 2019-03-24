e9aae6f8af201ece4c38eb9668c0857ffc4cf79a
rsmease <rsmease@gmail.com>
2019-03-24 11:16:38 -0700

    Add productIterator

---
+++ algorithms/javascript/productIterator.js
@@ -0,0 +1,21 @@
+const productIterator = (base, end) => {
+  if (base === end) {
+    return end;
+  }
+  return times(base, productIterator(base + 1, end));
+}
+
+const times = (a, b) => {
+  if (a === 0 || b === 0) {
+    return 0;
+  }
+  if (b % 2 === 0) {
+    return times(a + a, b / 2);
+  }
+  return a + times(a, b - 1);
+}
+
+const multiplyRange = (a, b) => productIterator(a, b);
+const factorial = (n) => multiplyRange(1, n);
+
+
