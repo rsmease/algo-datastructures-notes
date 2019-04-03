b97800f191f45d847f6bfa609fec125a6754ad82
rsmease <rsmease@gmail.com>
2019-04-03 16:00:34 -0700

    add perfect squares solution

--- algorithms/javascript/perfectSquares.js
+++ algorithms/javascript/perfectSquares.js
@@ -1,42 +1,18 @@
-var numSquares = function (desiredSum, count = 0, squares) {
-  const availableSquares = squares || getSquaresUpTo(desiredSum);
-
-  if (desiredSum < 0 || !availableSquares.length) {
-    return Number.MAX_SAFE_INTEGER;
+const numSquares = function (desiredSum) {
+  if (isSquare(desiredSum)) {
+    return 1;
   }
 
-  if (desiredSum === 0 || isSquare(desiredSum)) {
-    return count;
+  const memo = [0, 1, 2, 3];
+
+  for (let i = 3; i <= desiredSum; i++) {
+    memo[i] = memo[i - 1] + 1;
+    for (let j = 1; j * j <= i; j++) {
+      memo[i] = Math.min(memo[i], memo[i - j * j] + 1);
+    }
   }
 
-  const remainingSquares = availableSquares.slice(0);
-  const largestSquare = remainingSquares.pop();
-
-  return Math.min(
-    numSquares(
-      desiredSum - largestSquare,
-      count + 1,
-      availableSquares
-    ),
-    numSquares(
-      desiredSum,
-      count + 1,
-      remainingSquares
-    )
-  );
+  return memo[desiredSum]
 };
 
-var getSquaresUpTo = (limit) => {
-  const squares = [];
-  for (let i = 1; i * i < limit; i++) {
-    squares.push(i * i)
-  }
-  return squares;
-}
-
-var isSquare = (number) => {
-  const root = Math.pow(number, 0.5);
-  return Math.ceil(root) === Math.floor(root);
-}
-
-console.log(numSquares(12))
+const isSquare = (sum) => Math.sqrt(sum) % 1 === 0;
