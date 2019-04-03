da2ed7c72df1900e3bd59de2e7ba823dd906c466
rsmease <rsmease@gmail.com>
2019-04-03 16:06:03 -0700

    add square flag to speed up some actions

--- algorithms/javascript/perfectSquares.js
+++ algorithms/javascript/perfectSquares.js
@@ -4,11 +4,22 @@ const numSquares = function (desiredSum) {
   }
 
   const memo = [0, 1, 2, 3];
+  let squareFlag = false;
 
   for (let i = 3; i <= desiredSum; i++) {
     memo[i] = memo[i - 1] + 1;
+
+    if (squareFlag) {
+      squareFlag = false;
+      continue;
+    }
+
     for (let j = 1; j * j <= i; j++) {
       memo[i] = Math.min(memo[i], memo[i - j * j] + 1);
+    }
+
+    if (memo[i] === 1) {
+      squareFlag = true;
     }
   }
 
