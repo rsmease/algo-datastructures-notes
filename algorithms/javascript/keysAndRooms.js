876f621018d14c4dd3837eeab729873eb7212b4b
rsmease <rsmease@gmail.com>
2019-04-10 09:14:18 -0700

    working keys and rooms

--- algorithms/javascript/keysAndRooms.js
+++ algorithms/javascript/keysAndRooms.js
@@ -1,40 +1,20 @@
 const canVisitAllRooms = (rooms) => {
-  const roomsWithKnownAccess = new Set();
+  const roomsVisited = new Set();
+  const roomsToVisit = [];
 
-  let allRoomsTotal = 0;
-  let deadEndRoomsTotal = 0;
+  roomsToVisit.push(0);
+  roomsVisited.add(0);
+
   let currentRoom;
-  for (let i = 0; i < rooms.length; i++) {
-    currentRoom = rooms[i];
-    if (!currentRoom.length) {
-      deadEndRoomsTotal++;
-    }
-    if (deadEndRoomsTotal > 1) {
-      return false;
-    }
-    allRoomsTotal += i;
+  while (roomsToVisit.length) {
+    currentRoom = rooms[roomsToVisit.pop()];
+
+    currentRoom.forEach((otherRoom) => {
+      !roomsVisited.has(otherRoom) && roomsToVisit.push(otherRoom);
+      roomsVisited.add(otherRoom);
+    });
   }
-
-  let roomsTotal = 0;
-  rooms.forEach((availableKeys, currentRoom) => {
-    let otherRoom;
-    if (!availableKeys.length) {
-      deadEndRoomsTotal++;
-      if (deadEndRoomsTotal > 1) {
-        return false;
-      }
-    }
-
-    for (let j = 0; j < availableKeys.length; j++) {
-      otherRoom = availableKeys[j]
-      if (!(roomsWithKnownAccess.has(otherRoom) || otherRoom === currentRoom)) {
-        roomsTotal += otherRoom;
-        roomsWithKnownAccess.add(otherRoom);
-      }
-    }
-  });
-
-  return roomsTotal === allRoomsTotal;
+  return rooms.length === roomsVisited.size;
 };
 
 console.log(canVisitAllRooms([[4], [3], [], [2, 5, 7], [1], [], [8, 9], [], [], [6]]));
