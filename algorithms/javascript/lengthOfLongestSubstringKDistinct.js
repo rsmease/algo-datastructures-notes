4671ffa863a39b4172fd0bd9969fdebc7f35c6b0
rsmease <rsmease@gmail.com>
2019-04-26 09:19:41 -0700

    Add empty files for five new problems

---
+++ algorithms/javascript/lengthOfLongestSubstringKDistinct.js
@@ -0,0 +1,26 @@
+/*
+Prompt
+
+Given a string, find the length of the longest substring T that contains at most k distinct characters.
+
+Example 1:
+
+Input: s = "eceba", k = 2
+Output: 3
+Explanation: T is "ece" which its length is 3.
+Example 2:
+
+Input: s = "aa", k = 1
+Output: 2
+Explanation: T is "aa" which its length is 2.
+*/
+
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+
+const lengthOfLongestSubstringKDistinct = (s, k) => {
+
+}
