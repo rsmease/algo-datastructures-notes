67851f5e34572f43cc4276ed5984171eec1d110b
rsmease <rsmease@gmail.com>
2019-05-20 13:09:50 -0700

    add longest palindrome solution

---
+++ algorithms/ruby/longest_palindrome.rb
@@ -0,0 +1,47 @@
+# Given a string which consists of lowercase or uppercase letters, find the
+# length of the longest palindromes that can be built with those letters.
+
+# This is case sensitive, for example "Aa" is not considered a palindrome here.
+
+# Note:
+# Assume the length of given string will not exceed 1,010.
+
+# Example:
+
+# Input:
+# "abccccdd"
+
+# Output:
+# 7
+
+# Explanation:
+# One longest palindrome that can be built is "dccaccd", whose length is 7.
+
+# @param {String} s
+# @return {Integer}
+
+def longest_palindrome(s)
+  characters = s.chars
+  char_frequency_map = Hash.new(0)
+
+  characters.each { |char| char_frequency_map[char] += 1 }
+
+  frequencies_sum = 0
+  middle_character = 0
+
+  char_frequency_map.each do |char, frequency|
+    (middle_character = 1) if middle_character.zero? && frequency.odd?
+
+    frequencies_sum += (frequency / 2).floor * 2
+  end
+
+  return frequencies_sum + middle_character
+end
+
+require 'set'
+
+tests = Set.new
+tests.add("abccccdd")
+tests.add("civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth")
+
+tests.each { |test| p longest_palindrome(test) }
